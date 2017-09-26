f625656af3a80baec29aac28680250a66af1926f
EC2 Default User <ec2-user@ip-172-31-7-130.ec2.internal>
2017-09-26 20:47:04 +0000

    Tidyed up code

--- fpl_scraper.py
+++ fpl_scraper.py
@@ -3,38 +3,51 @@ from bs4 import BeautifulSoup
 import json
 import MySQLdb
 import tables
-
-url = "https://fantasy.premierleague.com/drf/bootstrap-static"
-
-html = requests.get(url).text
-player_info = json.loads(html)
-db = MySQLdb.connect(host="testdb.ckbnf4kfx74t.us-east-1.rds.amazonaws.com",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="rootroot",  # your password
-                     db="test")        # name of the data base
-
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
-cur.execute("drop table if exists players")
-sql_str = "CREATE TABLE  players (" + tables.table_str + ");"
-cur.execute(sql_str)
-
-for idx,player in enumerate(player_info["elements"]):
-    args = idx, player['first_name'], player['second_name'], player['goals_scored']
-    sql = "INSERT INTO players  VALUES (%s, %s, %s, %s)"
-
-    placeholders = ', '.join(['%s'] * len(player))
-    columns = ', '.join(player.keys())
-    sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % ("players", columns, placeholders)
-    cur.execute(sql, player.values()) 
+import yaml
 
 
-cur.execute("SELECT * FROM players WHERE first_name = 'Harry' AND second_name = 'Kane'")
+with open("secret_config.yaml", 'r') as stream:
+    try:
+        config = (yaml.load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
+class SQL:
+    def SQL_Connect(self):
+    
+        self.db = MySQLdb.connect(host="testdb.ckbnf4kfx74t.us-east-1.rds.amazonaws.com",    
+                     user=config["SQL_User"],    
+                     passwd=config["SQL_Pass"],  
+                     db="test")        
 
-rows = cur.fetchall()
-col_names = [i[0] for i in cur.description]
-for item in rows:
-    for col, val in zip(item,col_names):
-        print str(val) + ": " + str(col)
-db.close()
+        cur = self.db.cursor()
+        return cur
+
+    def SQL_Close(self):
+        self.db.close()
+    
+def main():
+    sql = SQL()
+    cur = sql.SQL_Connect()
+    url = "https://fantasy.premierleague.com/drf/bootstrap-static"
+    cur.execute("DROP TABLE IF EXISTS players")
+ 
+    html = requests.get(url).text
+    player_info = json.loads(html)
+
+    sql_str = "CREATE TABLE  players (" + tables.table_str + ");"
+    cur.execute(sql_str)
+
+    for idx,player in enumerate(player_info["elements"]):
+        args = idx, player['first_name'], player['second_name'], player['goals_scored']
+        sql_str = "INSERT INTO players  VALUES (%s, %s, %s, %s)"
+
+        placeholders = ', '.join(['%s'] * len(player))
+        columns = ', '.join(player.keys())
+        sql_str = "INSERT INTO {} ( {} ) VALUES ( {} )".format("players", columns, placeholders)
+        cur.execute(sql_str, player.values()) 
+
+    sql.SQL_Close()
+
+
+if __name__ == '__main__':
+    main()
