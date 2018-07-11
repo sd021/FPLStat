295877d67cd006f9db548d90723a8f95114bc339
EC2 Default User <ec2-user@ip-172-31-7-130.ec2.internal>
2018-07-11 15:17:03 +0000

    Redesigned approach

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FPLStat
+todo:
+visualise players points over time
+create html table of data @ .com
+get rest of data
+db management (new tables, security)
+correlation graphs of data with pandas
+begin ML models to data
+
--- fpl_scraper.py
+++ fpl_scraper.py
@@ -1,53 +1,113 @@
 import requests
-from bs4 import BeautifulSoup
 import json
-import MySQLdb
-import tables
 import yaml
+import pandas as pd
+import numpy as np
+
+overview_url = "https://fantasy.premierleague.com/drf/bootstrap-static"
+base_pinfo_url = "https://fantasy.premierleague.com/drf/element-summary/"
+teams_url = "https://fantasy.premierleague.com/drf/teams/"
+
+class FPLScraper(object):
+    def __init__(self, *args):
+        self.load_data()
+
+    # Load some of the data into memory to reduce number of GET api calls
+    def load_data(self):
+        html=requests.get(teams_url).text
+        self.teams_info=json.loads(html)
+
+        html=requests.get(overview_url).text
+        self.overview_info=json.loads(html)
+
+    # Create a dictionary to relate team codes to team names
+    def create_team_info_dict(self):
+        team_dict = {}
+
+        for team in self.teams_info:
+            team_dict[team["code"]] = team["name"]
+        return team_dict
+
+    # Create a dictionary of static info on each player
+    def create_player_info_dict(self):
+        big_dict = {}
+
+        for player_json in self.overview_info["elements"]:
+            player_info_dict = {}
+            player_name = (player_json['first_name'] + " " +
+                        player_json['second_name']).encode('utf-8')
+
+            team_dict = self.create_team_info_dict()
+
+            player_info_dict["name"] = player_name
+            player_info_dict["value"] = player_json['now_cost'] / 10
+            player_info_dict["team"] = team_dict[player_json["team_code"]]
+            player_info_dict["goals_scored"] = player_json['goals_scored']
+            player_info_dict["goals_conceded"] = player_json['goals_conceded']
+            player_info_dict["total_points"] = player_json['total_points']
+            player_info_dict["form"] = player_json['form']
+            player_info_dict["assists"] = player_json['assists']
+            player_info_dict["selected_by_percent"] = player_json['selected_by_percent']
+            player_info_dict["bonus"] = player_json['bonus']
 
 
-with open("secret_config.yaml", 'r') as stream:
-    try:
-        config = (yaml.load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
-class SQL:
-    def SQL_Connect(self):
+            big_dict[player_json['id']] = player_info_dict
+
+        return big_dict
+
+    # Create a dictionary for of a given attribute across all gameweeks
+    def create_player_stats_dict(self, player_id, attribute):
+        html=requests.get(base_pinfo_url + player_id).text
+        player_info_json=json.loads(html)
+
+        ret_dict = {}
+        for gw in player_info_json["history"]:
+            round = gw["round"]
+            ret_dict[round] = float(gw[attribute])
+        return ret_dict
+
+    def create_player_dict(self):
+        big_dict = {}
+
+        for idx, player in enumerate(self.overview_info["elements"]):
+            player_dict = {}
+
+            p_id = str(player['id'])
+
+            player_dict["points"] = self.create_player_stats_dict(p_id, "total_points")
+            player_dict["ict"] = self.create_player_stats_dict(p_id, "ict_index")
+
+            big_dict[player['id']] = player_dict
+
+            if idx > 10:
+                break
+        
+        return big_dict
+
+    def create_df_from_dict(self, in_dict):
+        df = pd.DataFrame.from_dict({(i, j): in_dict[i][j]
+                                        for i in in_dict.keys()
+                                        for j in in_dict[i].keys()},
+                                    orient='index')
     
-        self.db = MySQLdb.connect(host="testdb.ckbnf4kfx74t.us-east-1.rds.amazonaws.com",    
-                     user=config["SQL_User"],    
-                     passwd=config["SQL_Pass"],  
-                     db="test")        
+        return df
 
-        cur = self.db.cursor()
-        return cur
-
-    def SQL_Close(self):
-        self.db.close()
-    
 def main():
-    sql = SQL()
-    cur = sql.SQL_Connect()
-    url = "https://fantasy.premierleague.com/drf/bootstrap-static"
-    cur.execute("DROP TABLE IF EXISTS players")
- 
-    html = requests.get(url).text
-    player_info = json.loads(html)
+    fpl_scraper = FPLScraper()
+    
+    big_dict = fpl_scraper.create_player_dict()
+    player_stats_df = fpl_scraper.create_df_from_dict(big_dict)
+    player_stats_df.to_csv("out.csv")
 
-    sql_str = "CREATE TABLE  players (" + tables.table_str + ");"
-    cur.execute(sql_str)
+   # player_stats_df = pd.read_csv("out.csv", index_col=[0, 1], skipinitialspace=True)
 
-    for idx,player in enumerate(player_info["elements"]):
-        args = idx, player['first_name'], player['second_name'], player['goals_scored']
-        sql_str = "INSERT INTO players  VALUES (%s, %s, %s, %s)"
+    player_info_dict = fpl_scraper.create_player_info_dict()
 
-        placeholders = ', '.join(['%s'] * len(player))
-        columns = ', '.join(player.keys())
-        sql_str = "INSERT INTO {} ( {} ) VALUES ( {} )".format("players", columns, placeholders)
-        cur.execute(sql_str, player.values()) 
+    print player_stats_df.T
 
-    sql.SQL_Close()
-
+    for idx in player_stats_df.index.levels[0]:
+        pass
+        #print player_info_dict[idx]["name"] + " " + str(player_stats_df.loc[(idx, "points")].corr(player_stats_df.loc[(idx, "ict")]))
 
 if __name__ == '__main__':
     main()
